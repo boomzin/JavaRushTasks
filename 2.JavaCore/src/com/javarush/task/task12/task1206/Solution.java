3a423ba1be5cc876a75bb05426cedcda8ee554a5
boomzinWork <boomzin@gmail.com>
2019-10-10 15:58:40 +0300

    WorkPlace20191010

--- 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -8,6 +8,14 @@ public class Solution {
     public static void main(String[] args) {
 
     }
+    public static String print(String s){
+        return s;
+
+    }
+    public static String print(int i){
+        return Integer.toString(i);
+
+    }
 
     //Напишите тут ваши методы
 }
