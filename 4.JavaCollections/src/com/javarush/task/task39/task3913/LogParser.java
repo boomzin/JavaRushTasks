06071ef47aa4f155c389ee09516357299cbc3c37
boomzinHoumePC <boomzin@gmail.com>
2020-12-02 11:31:24 +0300

    20201202home

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
@@ -7,71 +7,106 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LogParser implements IPQuery {
     private final List<String> logLines = new ArrayList<>();
 
-
     public LogParser(Path logDir) {
        File[] listFiles = logDir.toFile().listFiles();
-        for (File listFile : listFiles) {
-            if (listFile.isFile() && listFile.toString().toLowerCase().endsWith(".log")) {
-                try {
-                    logLines.addAll(Files.readAllLines(Paths.get(listFile.toURI())));
-                } catch (IOException e) {
-                    System.out.println("Reading log file error" + logLines);
-                }
+        if (listFiles != null) {
+            for (File listFile : listFiles) {
+                if (listFile.isFile() && listFile.toString().toLowerCase().endsWith(".log")) {
+                    try {
+                        logLines.addAll(Files.readAllLines(Paths.get(listFile.toURI())));
+                    } catch (IOException e) {
+                        System.out.println("Reading log file error" + logLines);
+                    }
 
+                }
             }
         }
     }
 
-    private Stream selectOnDate(Date after, Date before, String key) {
-        Stream<String> allLines = logLines.stream();
+    private Set<String> selectKeyOnDate(Date after, Date before, String key, KeyType keyType) {
+        Set<String> selectedIPsOnDate;
+        Stream<String> stringStream = logLines.stream();
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.ENGLISH);
         if (after == null && before == null) {
-            return allLines;
+            after = new Date(0L);
+            before = new Date(Long.MAX_VALUE);
         }
 
         if (after == null) {
             after = new Date();
-            after.setTime(Long.MIN_VALUE);
+            after.setTime(0L);
         }
         if (before == null) {
             before = new Date();
             before.setTime(Long.MAX_VALUE);
         }
+        Stream<String> stringStreamOnKey;
+        switch (keyType) {
+            case USER:
+                stringStreamOnKey = stringStream.filter(x -> (x.split("\t")[1]).contains(key));
+                break;
+            case EVENT:
+                stringStreamOnKey = stringStream.filter(x -> (x.split("\t")[3]).contains(key));
+                break;
+            case STATUS:
+                stringStreamOnKey = stringStream.filter(x -> (x.split("\t")[4]).contains(key));
+                break;
+            default:
+                stringStreamOnKey = stringStream;
+        }
+        Date finalBefore = before;
+        Date finalAfter = after;
+        selectedIPsOnDate = stringStreamOnKey
+                .filter(x -> {
+                    try {
+                        return format.parse(x.split("\t")[2]).getTime() <= finalBefore.getTime() &&
+                                format.parse(x.split("\t")[2]).getTime() >= finalAfter.getTime();
+                    } catch (ParseException e) {
+                        System.out.println("Date format parsing error");
+                        return false;
+                    }
+                })
+                .map(x -> x.split("\t")[0])
+                .collect(Collectors.toSet());
 
-
-
-        return null;
+        return selectedIPsOnDate;
     }
-
 
     @Override
     public int getNumberOfUniqueIPs(Date after, Date before) {
-        System.out.println(logLines);
-        return 0;
+        return selectKeyOnDate(after, before, "", KeyType.OTHER).size();
     }
 
     @Override
     public Set<String> getUniqueIPs(Date after, Date before) {
-        return null;
+        return selectKeyOnDate(after, before, "", KeyType.OTHER);
     }
 
     @Override
     public Set<String> getIPsForUser(String user, Date after, Date before) {
-        return null;
+        return selectKeyOnDate(after, before, user, KeyType.USER);
     }
 
     @Override
     public Set<String> getIPsForEvent(Event event, Date after, Date before) {
-        return null;
+        return selectKeyOnDate(after, before, event.name(), KeyType.EVENT);
     }
 
     @Override
     public Set<String> getIPsForStatus(Status status, Date after, Date before) {
-        return null;
+        return selectKeyOnDate(after, before, status.toString(), KeyType.STATUS);
     }
+
+    public enum KeyType {USER, EVENT, STATUS, OTHER}
+
 }
--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -1,10 +1,9 @@
 package com.javarush.task.task39.task3913;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         LogParser logParser = new LogParser(Paths.get("c:/logs/"));
         System.out.println(logParser.getNumberOfUniqueIPs(null, null));
         System.out.println(logParser.getUniqueIPs(null, null));
