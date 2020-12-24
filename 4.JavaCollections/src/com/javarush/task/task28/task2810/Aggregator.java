ec484afbf6a43f02bebb5592177a77f3e233d70a
boomzinHoumePC <boomzin@gmail.com>
2020-12-24 14:51:14 +0300

    20201224home

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -2,15 +2,17 @@ package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.HHStrategy;
 import com.javarush.task.task28.task2810.model.Model;
+import com.javarush.task.task28.task2810.model.MoikrugStrategy;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.view.HtmlView;
 
 public class Aggregator {
 
     public static void main(String[] args) {
-        Provider provider = new Provider(new HHStrategy());
+        Provider hhProvider = new Provider(new HHStrategy());
+        Provider mkProvider = new Provider(new MoikrugStrategy());
         HtmlView htmlView = new HtmlView();
-        Model model = new Model(htmlView, provider);
+        Model model = new Model(htmlView, hhProvider, mkProvider);
         Controller controller = new Controller(model);
         htmlView.setController(controller);
         htmlView.userCitySelectEmulationMethod();
