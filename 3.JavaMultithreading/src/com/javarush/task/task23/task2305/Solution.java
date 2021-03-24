183d22574671030a6adc23ffc3ae3b3981e0a61e
boomzinWork <boomzin@gmail.com>
2021-03-24 16:28:47 +0300

    WorkPlace20210324

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task23.task2305;
+
+/* 
+Inner
+*/
+
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+        Solution solution = new Solution();
+        solution.innerClasses[0] = solution.new InnerClass();
+        solution.innerClasses[1] = solution.new InnerClass();
+        Solution solution1 = new Solution();
+        solution1.innerClasses[0] = solution1.new InnerClass();
+        solution1.innerClasses[1] = solution1.new InnerClass();
+        return new Solution[] {solution, solution1};
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
